caec0d56f6d8494389ca08640ece9773ee251d6a
Zon <228657348@qq.com>
2020-11-16 23:05:21 +0800

    分类

--- src/pages/home/home-category/index.vue
+++ src/pages/home/home-category/index.vue
@@ -1,17 +1,79 @@
 <template>
-  <view>
-    分类
+  <view class="home-category">
+    <navigator
+      :url="`/pages/imgCategory/index?id=${item.id}`"
+      class="category-item"
+      v-for="item in category"
+      :key="item.id"
+    >
+      <image mode="aspectFill" :src="item.cover"></image>
+      <text class="category-name">{{ item.name }}</text>
+    </navigator>
   </view>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      category: []
+    };
+  },
   mounted() {
     uni.setNavigationBarTitle({
       title: "分类"
     });
+
+    this.getList();
+  },
+  methods: {
+    getList() {
+      this.request({
+        url: "http://157.122.54.189:9088/image/v1/vertical/category"
+      })
+        .then(result => {
+          console.log(result);
+          this.category = result.res.category;
+        })
+        .catch(err => {});
+    }
   }
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.home-category {
+  display: flex;
+  flex-wrap: wrap;
+  .category-item {
+    width: 33.33%;
+    height: calc(750rpx / 3);
+    position: relative;
+    border: 4rpx solid #fff;
+    image {
+      width: 100%;
+      height: 100%;
+    }
+
+    .category-name {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      padding-left: 20rpx;
+      color: #fff;
+      // font-size: 30rpx;
+
+      width: 100%;
+      height: 40rpx;
+      line-height: 40rpx;
+
+      // 渐变
+      background-image: linear-gradient(
+        to right top,
+        rgba(0, 0, 0, 0.4),
+        rgba(0, 0, 0, 0)
+      );
+    }
+  }
+}
+</style>
---
+++ src/pages/imgCategory/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <view id="category">
+    <view class="category-tab">
+      <view class="category-title">
+        <view class="title-inner">
+          <uni-segmented-control
+            :current="current"
+            :values="items.map(v => v.title)"
+            @clickItem="onClickItem"
+            style-type="text"
+            active-color="#d4237a"
+          ></uni-segmented-control>
+        </view>
+      </view>
+      <view class="category-content">
+        <view v-if="current === 0">
+          1
+        </view>
+        <view v-if="current === 1">
+          2
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+import { uniSegmentedControl } from "@dcloudio/uni-ui";
+export default {
+  components: {
+    uniSegmentedControl
+  },
+  data() {
+    return {
+      items: [{ title: "最新" }, { title: "热门" }],
+      current: 0
+    };
+  },
+  methods: {
+    onClickItem(e) {
+      if (this.current !== e.currentIndex) {
+        this.current = e.currentIndex;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#category {
+  .category-tab {
+    .category-title {
+      position: relative;
+      .title-inner {
+        width: 60%;
+        margin: 0 auto;
+      }
+      .iconsearch {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        right: 5%;
+      }
+    }
+    .category-content {
+    }
+  }
+}
+</style>
